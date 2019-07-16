4a9a32098905208a49128b6ad9637c7683c4a869
Evaldo Jr <evaldojunior1997@hotmail.com>
2019-07-16 18:21:36 -0300

    Implementando tela de listagem clientes

---
+++ customers/functions.php
@@ -0,0 +1,15 @@
+<?php
+
+require_once('../config.php');
+require_once(DBAPI);
+
+$customers = null;
+$customer = null;
+
+/**
+ *  Listagem de Clientes
+ */
+function index() {
+	global $customers;
+	$customers = find_all('customers');
+}
--- inc/database.php
+++ inc/database.php
@@ -20,4 +20,54 @@ function close_database($conn) {
 	}
 }
 
+/**
+ *  Pesquisa um Registro pelo ID em uma Tabela
+ */
+function find( $table = null, $id = null ) {
+  
+	$database = open_database();
+	$found = null;
+
+	try {
+	  if ($id) {
+	    $sql = "SELECT * FROM " . $table . " WHERE id = " . $id;
+	    $result = $database->query($sql);
+	    
+	    if ($result->num_rows > 0) {
+	      $found = $result->fetch_assoc();
+	    }
+	    
+	  } else {
+	    
+	    $sql = "SELECT * FROM " . $table;
+	    $result = $database->query($sql);
+	    
+	    if ($result->num_rows > 0) {
+	      $found = $result->fetch_all(MYSQLI_ASSOC);
+        
+        /* Metodo alternativo
+        $found = array();
+
+        while ($row = $result->fetch_assoc()) {
+          array_push($found, $row);
+        } */
+	    }
+	  }
+	} catch (Exception $e) {
+	  $_SESSION['message'] = $e->GetMessage();
+	  $_SESSION['type'] = 'danger';
+  }
+	
+	close_database($database);
+	return $found;
+}
+
+/**
+ *  Pesquisa Todos os Registros de uma Tabela
+ */
+function find_all( $table ) {
+	return find($table);
+  }
+
+
 ?>
